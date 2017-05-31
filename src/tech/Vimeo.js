dac787736c58be794d6667db8c09aede53d4e9c7
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2017-05-31 22:28:01 +0200

    Apply editorconfig
    Uses 2 spaces instead of 4
    
    LoveIsGrief/videojs-externals#5 Test vimeo

--- src/tech/Vimeo.js
+++ src/tech/Vimeo.js
@@ -21,281 +21,281 @@ const Tech = videojs.getComponent('Tech');
  */
 
 class Vimeo extends Externals {
-    constructor (options, ready) {
-        super(options, ready);
+  constructor(options, ready) {
+    super(options, ready);
+  }
+
+  createEl() {
+
+    let vimeoSource = null;
+    if ('string' === typeof this.options_.source) {
+      vimeoSource = this.options_.source;
+    }
+    else if ('object' === typeof this.options_.source) {
+      vimeoSource = this.options_.source.src;
     }
 
-    createEl () {
+    this.src_ = this.parseSrc(vimeoSource);
 
-        let vimeoSource = null;
-        if ('string' === typeof this.options_.source) {
-            vimeoSource = this.options_.source;
+    const el_ = super.createEl('iframe', {
+      id: this.options_.techId,
+      src: `${this.options_.embed}/${this.src_}??api=1&player_id=${this.options_.techId}&fullscreen=1&autoplay=${this.options_.autoplay}`
+    });
+
+    videojs(this.options_.playerId);
+    return el_;
+
+  }
+
+
+  parseSrc(src) {
+    if (src) {
+      // Regex that parse the video ID for any Vimeo URL
+      var regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
+      var match = src.match(regExp);
+
+      if (match && match[5]) {
+        return match[5];
+      }
+    }
+  }
+
+  setSrc(src) {
+    this.widgetPlayer.loadVideo(this.parseSrc(src)).then((id) => {
+      this.src_ = id;
+    });
+  }
+
+  isApiReady() {
+    return window['Vimeo'] && window['Vimeo']['Player'];
+  }
+
+  addScriptTag() {
+    var self = this;
+    if (window['requirejs']) {
+      let requirejs = window['requirejs'];
+      requirejs([this.options_.api], function (Vimeo) {
+        window['Vimeo'] = {Player: Vimeo};
+        self.initTech();
+      });
+    } else {
+      var r = false,
+        d = document,
+        s = d.getElementsByTagName('head')[0] || d.documentElement;
+      var js = d.createElement('script');
+      js.async = true;
+      js.type = 'text/javascript';
+      js.onload = js.onreadystatechange = function () {
+        var rs = this.readyState;
+        if (!r && (!rs || /loaded|complete/.test(rs))) {
+          r = true;
+          // Handle memory leak in IE
+          js.onload = js.onreadystatechange = null;
+          self.initTech();
         }
-        else if ('object' === typeof this.options_.source) {
-            vimeoSource = this.options_.source.src;
-        }
+      };
 
-        this.src_ = this.parseSrc(vimeoSource);
+      js.src = this.options_.api;
+      s.insertBefore(js, s.firstChild);
+    }
+  }
 
-        const el_ = super.createEl('iframe', {
-            id: this.options_.techId,
-            src: `${this.options_.embed}/${this.src_}??api=1&player_id=${this.options_.techId}&fullscreen=1&autoplay=${this.options_.autoplay}`
-        });
-
-        videojs(this.options_.playerId);
-        return el_;
-
+  initTech() {
+    if (!this.isApiReady()) {
+      return;
+    }
+    let source = null;
+    if ('string' === typeof this.options_.source) {
+      source = this.options_.source;
+    }
+    else if ('object' === typeof this.options_.source) {
+      source = this.options_.source.src;
     }
 
+    source = this.parseSrc(source);
 
-    parseSrc (src) {
-        if (src) {
-            // Regex that parse the video ID for any Vimeo URL
-            var regExp = /^.*(vimeo\.com\/)((channels\/[A-z]+\/)|(groups\/[A-z]+\/videos\/))?([0-9]+)/;
-            var match = src.match(regExp);
+    const vimOpts = videojs.mergeOptions(this.options_, {
+      id: source,
+      byline: 0,
+      color: '#00adef',
+      portrait: 0,
+      fullscreen: 1
+    });
 
-            if (match && match[5]) {
-                return match[5];
-            }
-        }
-    }
+    this.widgetPlayer = new window.Vimeo.Player(this.options_.techId, vimOpts);
+    this.widgetPlayer.ready().then(videojs.bind(this, this.onReady));
+    super.initTech();
+    this.onStateChange({type: -1});
+  }
 
-    setSrc(src) {
-      this.widgetPlayer.loadVideo(this.parseSrc(src)).then((id)=>{
-        this.src_ = id;
+  onReady() {
+    super.onReady();
+    this.onStateChange({type: 'loaded'});
+  }
+
+  setupTriggers() {
+    this.widgetPlayer.vjsTech = this;
+    for (var i = Vimeo.Events.length - 1; i >= 0; i--) {
+      const eventName = Vimeo.Events[i];
+      /*jshint loopfunc: true */
+      this.widgetPlayer.on(eventName, (data) => {
+        this.eventHandler(videojs.mergeOptions({type: eventName}, data));
       });
     }
+  }
 
-    isApiReady () {
-        return window['Vimeo'] && window['Vimeo']['Player'];
+  onStateChange(event) {
+    let state = event.type;
+    this.lastState = state;
+    // console.debug("event.type:", state);
+    if (event.volume) {
+      this.updateVolume();
     }
-
-    addScriptTag() {
-        var self = this;
-        if(window['requirejs']) {
-            let requirejs = window['requirejs'];
-            requirejs([this.options_.api], function (Vimeo) {
-                window['Vimeo'] = {Player: Vimeo};
-                self.initTech();
-            });
-        } else {
-            var r = false,
-                d = document,
-                s = d.getElementsByTagName('head')[0] || d.documentElement;
-            var js = d.createElement('script');
-            js.async = true;
-            js.type = 'text/javascript';
-            js.onload = js.onreadystatechange = function () {
-                var rs = this.readyState;
-                if (!r && (!rs || /loaded|complete/.test(rs))) {
-                    r = true;
-                    // Handle memory leak in IE
-                    js.onload = js.onreadystatechange = null;
-                    self.initTech();
-                }
-            };
-
-            js.src = this.options_.api;
-            s.insertBefore(js, s.firstChild);
+    if (event.duration && this.duration_ !== event.duration) {
+      this.duration_ = event.duration;
+      this.trigger('durationchange');
+    }
+    switch (state) {
+      case 'loaded':
+        this.trigger('loadedmetadata');
+        this.trigger('durationchange');
+        this.trigger('canplay');
+        break;
+      case 'timeupdate':
+        if (event.seconds) {
+          this.currentTime_ = event.seconds;
+          this.trigger('timeupdate');
+          this.trigger('playing');
         }
-    }
-
-    initTech () {
-        if (!this.isApiReady()) {
-            return;
+        break;
+      case 'progress':
+        if (event.percent) {
+          this.buffered_ = event.percent;
+          this.trigger('progress');
         }
-        let source = null;
-        if ('string' === typeof this.options_.source) {
-            source = this.options_.source;
+        break;
+      case 'pause':
+        this.paused_ = true;
+        this.trigger('pause');
+        break;
+      case 'play':
+        this.paused_ = false;
+        this.trigger('play');
+        break;
+      case 'end':
+        this.updateEnded();
+        break;
+      case 'error':
+        this.onPlayerError(event);
+        break;
+      default:
+        super.onStateChange(event);
+    }
+    this.updatePaused();
+  }
+
+  updateVolume() {
+    this.widgetPlayer.getVolume().then((volume) => {
+      this.volume_ = volume;
+      if (this.volume_ !== volume) {
+        this.trigger('volumechange');
+      }
+    });
+  }
+
+  updateEnded() {
+    this.widgetPlayer.getEnded().then((ended) => {
+      this.ended_ = ended;
+      if (ended) {
+        this.trigger('ended');
+      }
+    });
+  }
+
+  updatePaused() {
+    this.widgetPlayer.getPaused().then((paused) => {
+      if (paused !== this.paused_) {
+        this.paused_ = paused;
+        if (paused) {
+          this.trigger('pause');
         }
-        else if ('object' === typeof this.options_.source) {
-            source = this.options_.source.src;
-        }
+      }
+    });
+  }
 
-        source = this.parseSrc(source);
+  updateDuration() {
+    this.widgetPlayer.getDuration().then((duration) => {
+      this.duration_ = duration;
+    });
+  }
 
-        const vimOpts = videojs.mergeOptions(this.options_, {
-            id: source,
-            byline: 0,
-            color: '#00adef',
-            portrait: 0,
-            fullscreen: 1
-        });
+  buffered() {
+    return videojs.createTimeRange(0, (this.buffered_ * this.duration_) || 0);
+  }
 
-        this.widgetPlayer = new window.Vimeo.Player(this.options_.techId, vimOpts);
-        this.widgetPlayer.ready().then(videojs.bind(this, this.onReady));
-        super.initTech();
-        this.onStateChange({type: -1});
+  ended() {
+    return this.ended_;
+  }
+
+  duration() {
+    return this.duration_;
+  }
+
+  currentTime() {
+    return this.currentTime_;
+  }
+
+
+  setCurrentTime(seconds) {
+    this.trigger('seeking');
+    this.widgetPlayer.setCurrentTime(seconds).then((seconds) => {
+      this.currentTime_ = seconds;
+      this.trigger('timeupdate');
+      this.trigger('seeked');
+    });
+  }
+
+  play() {
+    this.widgetPlayer.play();
+  }
+
+  pause() {
+    this.widgetPlayer.pause();
+  }
+
+  paused() {
+    return this.paused_;
+  }
+
+  muted() {
+    return this.muted_;
+  }
+
+  volume() {
+    return this.volume_;
+  }
+
+  setVolume(percentAsDecimal) {
+    if (percentAsDecimal !== this.volume_) {
+      this.widgetPlayer.setVolume(percentAsDecimal).then(() => {
+        this.updateVolume();
+      });
     }
+  }
 
-    onReady () {
-        super.onReady();
-        this.onStateChange({type: 'loaded'});
+  setMuted(muted) {
+    this.muted_ = muted;
+    if (muted) {
+      this.volumeBefore_ = this.volume_;
     }
-
-    setupTriggers () {
-        this.widgetPlayer.vjsTech = this;
-        for (var i = Vimeo.Events.length - 1; i >= 0; i--) {
-            const eventName = Vimeo.Events[i];
-            /*jshint loopfunc: true */
-            this.widgetPlayer.on(eventName, (data) => {
-                this.eventHandler(videojs.mergeOptions({type: eventName}, data));
-            });
-        }
-    }
-
-    onStateChange (event) {
-        let state = event.type;
-        this.lastState = state;
-        // console.debug("event.type:", state);
-        if (event.volume) {
-            this.updateVolume();
-        }
-        if (event.duration && this.duration_ !== event.duration) {
-            this.duration_ = event.duration;
-            this.trigger('durationchange');
-        }
-        switch (state) {
-            case 'loaded':
-                this.trigger('loadedmetadata');
-                this.trigger('durationchange');
-                this.trigger('canplay');
-                break;
-            case 'timeupdate':
-                if (event.seconds) {
-                    this.currentTime_ = event.seconds;
-                    this.trigger('timeupdate');
-                    this.trigger('playing');
-                }
-                break;
-            case 'progress':
-                if (event.percent) {
-                    this.buffered_ = event.percent;
-                    this.trigger('progress');
-                }
-                break;
-            case 'pause':
-                this.paused_ = true;
-                this.trigger('pause');
-                break;
-            case 'play':
-                this.paused_ = false;
-                this.trigger('play');
-                break;
-            case 'end':
-                this.updateEnded();
-                break;
-            case 'error':
-                this.onPlayerError(event);
-                break;
-            default:
-              super.onStateChange(event);
-        }
-        this.updatePaused();
-    }
-
-    updateVolume () {
-        this.widgetPlayer.getVolume().then((volume)=> {
-            this.volume_ = volume;
-            if(this.volume_ !== volume) {
-                this.trigger('volumechange');
-            }
-        });
-    }
-
-    updateEnded () {
-        this.widgetPlayer.getEnded().then((ended)=> {
-            this.ended_ = ended;
-            if(ended) {
-                this.trigger('ended');
-            }
-        });
-    }
-
-    updatePaused () {
-        this.widgetPlayer.getPaused().then((paused)=> {
-            if(paused !== this.paused_) {
-                this.paused_ = paused;
-                if(paused) {
-                    this.trigger('pause');
-                }
-            }
-        });
-    }
-
-    updateDuration () {
-        this.widgetPlayer.getDuration().then((duration)=> {
-            this.duration_ = duration;
-        });
-    }
-
-    buffered () {
-        return videojs.createTimeRange(0, (this.buffered_ * this.duration_) || 0);
-    }
-
-    ended () {
-        return this.ended_;
-    }
-
-    duration () {
-        return this.duration_;
-    }
-
-    currentTime () {
-        return this.currentTime_;
-    }
-
-
-    setCurrentTime (seconds) {
-        this.trigger('seeking');
-        this.widgetPlayer.setCurrentTime(seconds).then((seconds)=> {
-            this.currentTime_ = seconds;
-            this.trigger('timeupdate');
-            this.trigger('seeked');
-        });
-    }
-
-    play () {
-        this.widgetPlayer.play();
-    }
-
-    pause () {
-        this.widgetPlayer.pause();
-    }
-
-    paused () {
-        return this.paused_;
-    }
-
-    muted () {
-        return this.muted_;
-    }
-
-    volume () {
-        return this.volume_;
-    }
-
-    setVolume (percentAsDecimal) {
-        if (percentAsDecimal !== this.volume_) {
-            this.widgetPlayer.setVolume(percentAsDecimal).then(()=> {
-                this.updateVolume();
-            });
-        }
-    }
-
-    setMuted (muted) {
-        this.muted_ = muted;
-        if (muted) {
-            this.volumeBefore_ = this.volume_;
-        }
-        this.setVolume(muted ? 0 : this.volumeBefore_);
-    }
+    this.setVolume(muted ? 0 : this.volumeBefore_);
+  }
 }
 
 Vimeo.prototype.options_ = {
-    api: '//player.vimeo.com/api/player.js',
-    embed: '//player.vimeo.com/video',
-    visibility: 'visible'
+  api: '//player.vimeo.com/api/player.js',
+  embed: '//player.vimeo.com/video',
+  visibility: 'visible'
 };
 
 Vimeo.prototype.className_ = 'Vimeo';
@@ -303,7 +303,7 @@ Vimeo.prototype.className_ = 'Vimeo';
 /* Vimeo Support Testing -------------------------------------------------------- */
 
 Vimeo.isSupported = function () {
-    return true;
+  return true;
 };
 
 // Add Source Handler pattern functions to this tech
@@ -324,7 +324,7 @@ Vimeo.nativeSourceHandler = {};
  * @return {String}         'probably', 'maybe', or '' (empty string)
  */
 Vimeo.nativeSourceHandler.canPlayType = function (source) {
-    return (source.indexOf('vimeo') !== -1);
+  return (source.indexOf('vimeo') !== -1);
 };
 
 /*
@@ -335,18 +335,18 @@ Vimeo.nativeSourceHandler.canPlayType = function (source) {
  */
 Vimeo.nativeSourceHandler.canHandleSource = function (source) {
 
-    // If a type was provided we should rely on that
-    if (source.type) {
-        return Vimeo.nativeSourceHandler.canPlayType(source.type);
-    } else if (source.src) {
-        return Vimeo.nativeSourceHandler.canPlayType(source.src);
-    }
+  // If a type was provided we should rely on that
+  if (source.type) {
+    return Vimeo.nativeSourceHandler.canPlayType(source.type);
+  } else if (source.src) {
+    return Vimeo.nativeSourceHandler.canPlayType(source.src);
+  }
 
-    return '';
+  return '';
 };
 
 Vimeo.nativeSourceHandler.handleSource = function (source, tech) {
-    tech.src(source.src);
+  tech.src(source.src);
 };
 
 /*
