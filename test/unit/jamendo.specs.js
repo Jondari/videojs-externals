655dcd1f4ba2632dfc59ab20457f1cf8b61bdff4
LoveIsGrief <LoveIsGrief@users.noreply.github.com>
2017-10-10 23:11:16 +0200

    Greentest: Object source tests
    Nothing to do here :)
    
    LoveIsGrief/videojs-externals#19 Integrate Jamendo

--- test/unit/jamendo.specs.js
+++ test/unit/jamendo.specs.js
@@ -45,8 +45,8 @@ const stringSourceTestSuiteGenerator = new StringSourceTestSuiteGenerator(
 var testFactory = new MainTestFactory(basicConfiguration);
 
 // testFactory.addTestSuiteFactory(htmlSourceTestSuiteGenerator);
-testFactory.addTestSuiteFactory(noSourceTestSuiteGenerator);
-// testFactory.addTestSuiteFactory(objectSourceTestSuiteGenerator);
+// testFactory.addTestSuiteFactory(noSourceTestSuiteGenerator);
+testFactory.addTestSuiteFactory(objectSourceTestSuiteGenerator);
 // testFactory.addTestSuiteFactory(stringSourceTestSuiteGenerator);
 
 testFactory.generate();
